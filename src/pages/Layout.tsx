6009f8552b990e275edd7ae2f65216340c4f2d0a
Changho Youn <younch8342@yu.ac.kr>
2023-11-16 18:58:35 +0900

    navbar css 적용하기

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,26 +5,30 @@ import {FaUserCheck as LoginIcon} from "react-icons/fa";
 import {FaUserTimes as LogoutIcon} from "react-icons/fa";
 import {Outlet, useNavigate} from 'react-router-dom';
 import {Container, Navbar} from "react-bootstrap";
+import {IconButton} from "@mui/material";
 
 function Layout() {
     const navigate = useNavigate();
-    function goLoginPage(){
+
+    function goLoginPage() {
         navigate("/login");
     }
-    function goHomePage(){
+
+    function goHomePage() {
         navigate("/");
     }
+
     return (
         <div>
-            <Navbar className="navbar bg-body-tertiary justify-content-between">
-                <div className="home-btn" onClick={goHomePage}
-                >SHOP</div>
+            <Navbar className="bg-body-tertiary justify-content-between navbar">
+                <IconButton style={{marginLeft:"30px"}}><h2 className="home-btn" onClick={goHomePage}
+                >SHOP</h2></IconButton>
                 <div className="right-icons justify-content-around">
-                    <CartIcon className="cart-icon"/>
-                    <LoginPageIcon className="login-icon" fontSize="0.85em"
-                    onClick={goLoginPage}/>
-                    <LoginIcon className="lo-success-icon"/>
-                    <LogoutIcon className="lo-fail-icon"/>
+                    <IconButton><CartIcon className="cart-icon"/></IconButton>
+                    <IconButton><LoginPageIcon className="login-icon" fontSize="0.85em"
+                                               onClick={goLoginPage}/></IconButton>
+                    <IconButton><LoginIcon className="lo-success-icon"/></IconButton>
+                    <IconButton><LogoutIcon className="lo-fail-icon"/></IconButton>
                 </div>
             </Navbar>
             <main>
