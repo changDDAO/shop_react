0025804f738fadb0b59a9db85b718c5cb66a30fd
Changho Youn <younch8342@yu.ac.kr>
2023-11-19 10:14:44 +0900

    BasketStore 로직 수정

--- src/states/BasketStore.ts
+++ src/states/BasketStore.ts
@@ -1,38 +1,67 @@
 import {create} from 'zustand';
-import {Product} from "./DefinedType";
+import {BasketProduct, Product} from "./DefinedType";
 import useProductStore from "./ProductStore";
 import ProductStore from "./ProductStore";
 
 type BasketStore = {
-    inBasketList: Product[];
+    inBasketList: BasketProduct[];
     addProductBasket: (product: Product) => void;
+    addProductCount: (productId: number) => void;
+    minusProductCount: (productId: number) => void;
     removeProductBasket: (productId: number) => void;
     basketCount: number;
 }
 const BasketStore = create<BasketStore>((set) => ({
-    inBasketList: [] as Product[],
+    inBasketList: [],
     basketCount: 0,
-    addProductBasket: (product) => {
+    addProductBasket: (newProduct) => {
+        set((state) => {
+            const duplicateProduct = state.inBasketList.find(
+                (product) => product.id === newProduct.id
+            )
+            if (duplicateProduct) {
+                return {
+                    inBasketList: state.inBasketList.map(
+                        (p) => p.id === newProduct.id
+                            ? {...p, count: p.count + 1}
+                            : p)
+                }
+            } else {
+                return {
+                    inBasketList: [...state.inBasketList,
+                        {...newProduct, count: 1}],
+                }
+            }
+        })
 
-        set((state) => ({
-            inBasketList: [...state.inBasketList,
-                {
-                    id: product.id,
-                    title: product.title,
-                    price: product.price,
-                    description: product.description,
-                    category: product.category,
-                    image: product.image,
-                }],
-            basketCount: state.basketCount + 1,
-        }));
     },
     removeProductBasket: (productId: number) => {
         set((state) => ({
             inBasketList: [...state.inBasketList.filter(p => p.id !== productId)],
             basketCount: state.basketCount - 1,
         }));
-    }
+    },
+    addProductCount: (productId: number) => {
+        set((state) => ({
+            inBasketList: state.inBasketList.map((p) =>
+                (p.id === productId)
+                    ? {...p, count: p.count + 1}
+                    : p)
+        }))
+    },
+    minusProductCount: (productId: number) => {
+        set((state) => ({
+            inBasketList: state.inBasketList.map((p) =>
+                (p.id === productId)
+                    ? {
+                        ...p, count: p.count > 1
+                            ? p.count - 1
+                            : 1
+                    }
+                    : p)
+        }))
+    },
+
 }));
 
 export default BasketStore;
